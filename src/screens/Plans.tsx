fb095f519394dbf7c94c6787479975da5028fb01
Ananta Karmakar <codeantu@gmail.com>
2024-04-07 00:08:41 +0530

    change in plans page ->  added pending and Approved

--- src/screens/Plans.tsx
+++ src/screens/Plans.tsx
@@ -12,80 +12,104 @@ export default function Plans() {
         </div>
         <div className='flexCol gap-3'>
           <div className='productCard'>
-            <div className='productCardImgContainer'>
-              <img src='./icons/plan.png' alt='' className='productCardImg' />
-            </div>
-            <div className='flexCol flex-grow justify-between'>
-              <div className='productCardText'>Pic Automation</div>
-              <div className='productCardSubText  '>
-                <div className='flex justify-between'>
-                  <div>Day Income</div>
-                  <div>₹489</div>
-                </div>
-                <div className='flex justify-between'>
-                  <div>Earning Days</div>
-                  <div>49 Days</div>
-                </div>
-                <div className='flex justify-between'>
-                  <div>Total Income</div>
-                  <div>₹489</div>
-                </div>
-              </div>
-              <div className='textAccent flex justify-between'>
-                <div>Invest Amount</div>
-                <div>₹489</div>
+            <div className='w-1/2'>
+              <div className='status pending '>
+                <img src='./icons/pending.svg' alt='' className='statusImg' />
+                <div>Pending</div>
               </div>
             </div>
-          </div>
-          <div className='productCard'>
-            <div className='productCardImgContainer'>
-              <img src='./icons/plan.png' alt='' className='productCardImg' />
-            </div>
-            <div className='flexCol flex-grow justify-between'>
-              <div className='productCardText'>Pic Automation</div>
-              <div className='productCardSubText  '>
-                <div className='flex justify-between'>
-                  <div>Day Income</div>
-                  <div>₹489</div>
-                </div>
-                <div className='flex justify-between'>
-                  <div>Earning Days</div>
-                  <div>49 Days</div>
-                </div>
-                <div className='flex justify-between'>
-                  <div>Total Income</div>
-                  <div>₹489</div>
-                </div>
+            <div className='flex gap-3'>
+              <div className='productCardImgContainer'>
+                <img src='./icons/plan.png' alt='' className='productCardImg' />
               </div>
-              <div className='textAccent flex justify-between'>
-                <div>Invest Amount</div>
-                <div>₹489</div>
+              <div className='flexCol flex-grow justify-between'>
+                <div className='productCardText'>Pic Automation</div>
+                <div className='productCardSubText  '>
+                  <div className='flex justify-between'>
+                    <div>Day Income</div>
+                    <div>₹489</div>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div>Earning Days</div>
+                    <div>49 Days</div>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div>Total Income</div>
+                    <div>₹489</div>
+                  </div>
+                </div>
+                <div className='textAccent flex justify-between'>
+                  <div>Invest Amount</div>
+                  <div>₹489</div>
+                </div>
               </div>
             </div>
           </div>
           <div className='productCard'>
-            <div className='productCardImgContainer'>
-              <img src='./icons/plan.png' alt='' className='productCardImg' />
+            <div className='w-1/2'>
+              <div className='status approved'>
+                <img src='./icons/approved.svg' alt='' className='statusImg' />
+                <div>Approved</div>
+              </div>
             </div>
-            <div className='flexCol flex-grow justify-between'>
-              <div className='productCardText'>Pic Automation</div>
-              <div className='productCardSubText  '>
-                <div className='flex justify-between'>
-                  <div>Day Income</div>
-                  <div>₹489</div>
+            <div className='flex gap-3'>
+              <div className='productCardImgContainer'>
+                <img src='./icons/plan.png' alt='' className='productCardImg' />
+              </div>
+              <div className='flexCol flex-grow justify-between'>
+                <div className='productCardText'>Pic Automation</div>
+                <div className='productCardSubText  '>
+                  <div className='flex justify-between'>
+                    <div>Day Income</div>
+                    <div>₹489</div>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div>Earning Days</div>
+                    <div>49 Days</div>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div>Total Income</div>
+                    <div>₹489</div>
+                  </div>
                 </div>
-                <div className='flex justify-between'>
-                  <div>Earning Days</div>
-                  <div>49 Days</div>
-                </div>
-                <div className='flex justify-between'>
-                  <div>Total Income</div>
+                <div className='textAccent flex justify-between'>
+                  <div>Invest Amount</div>
                   <div>₹489</div>
                 </div>
               </div>
-              <div className='textAccent flex justify-between'>
-                <div>Invest Amount</div>
-                <div>₹489</div>
+            </div>
+          </div>
+          <div className='productCard'>
+            <div className='w-1/2'>
+              <div className='status rejected'>
+                <img src='./icons/rejected.svg' alt='' className='statusImg' />
+                <div>Rejected</div>
+              </div>
+            </div>
+            <div className='flex gap-3'>
+              <div className='productCardImgContainer'>
+                <img src='./icons/plan.png' alt='' className='productCardImg' />
+              </div>
+              <div className='flexCol flex-grow justify-between'>
+                <div className='productCardText'>Pic Automation</div>
+                <div className='productCardSubText  '>
+                  <div className='flex justify-between'>
+                    <div>Day Income</div>
+                    <div>₹489</div>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div>Earning Days</div>
+                    <div>49 Days</div>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div>Total Income</div>
+                    <div>₹489</div>
+                  </div>
+                </div>
+                <div className='textAccent flex justify-between'>
+                  <div>Invest Amount</div>
+                  <div>₹489</div>
+                </div>
               </div>
             </div>
           </div>
